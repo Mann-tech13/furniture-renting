9160dce4fcf9fbfc97c16168178637cc000c87a0
Mann-tech13 <mannshah.ict19@gmail.com>
2024-06-29 19:27:15 +0530

    api

--- client/src/apis/auth.js
+++ client/src/apis/auth.js
@@ -1,15 +1,20 @@
 import axios from 'axios';
 
 export async function register(data){
-    const api = await axios.post("http://localhost:3000/user/add", data)
-    console.log(api);
+    try {
+        const api = await axios.post("https://furniture-renting.onrender.com/user/add", data)
+        return api;
+    } catch (error) {
+        
+        console.log(error);
+    }
 }
 
 
 export async function login(data){
     try {
         console.log(data)
-        const api = await axios.post("http://localhost:3000/user/login", {
+        const api = await axios.post("https://furniture-renting.onrender.com/user/login", {
             email: data.email,
             password: data.password
         })
